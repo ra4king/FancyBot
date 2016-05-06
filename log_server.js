2a14b279e5bd4beaaf446188622ee7194d9fafe2
Roi Atalla <roi@roiatalla.com>
2016-05-06 07:32:53 -0400

    Added error checking.

--- log_server.js
+++ log_server.js
@@ -32,60 +32,64 @@ function log_request(request, response) {
             return;
         }
 
-        response.writeHead(200);
+        try {
+            var lines = data.toString().split('\n');
 
-        var lines = data.toString().split('\n');
-
-        var html = '';
-        html += '<html>\n';
-        html += '   <head>\n';
-        html += '       <title>#java-gaming logs ' + htmlencode.htmlEncode(date_string) + '</title>\n';
-        html += '       <style> body { line-height: 1.3em; background-color: #0C1010; color: #008000; font-family: "Consolas", "Source Code Pro", "Andale Mono", "Monaco", "Lucida Console", monospace; }\n';
-        html += '               a { color: #68A4DE; text-decoration: none; } a:hover { text-decoration: underline; }\n'
-        html += '               .msg { color: #FFFFFF; } .event { color: #BDB76B; } .nick { color: #DC143C; display: inline-block; text-align: right; min-width: 140px; max-width: 140px; padding-right: 10px; }</style>\n';
-        html += '   </head>\n';
-        html += '   <body>\n';
-        html += '       <h1>#java-gaming logs ' + htmlencode.htmlEncode(date_string) + '</h1>\n';
-        lines.forEach(function(s) {
-            var msg_regex = /^(\[.+?\])  (<.+?> )?(.+)$/;
-            var match = msg_regex.exec(s);
-            if(!match) {
-                return;
-            }
-
-            html += '       <div><span class="datestring">' + htmlencode.htmlEncode(match[1]) + '</span>  ';
-
-            var msg_class = 'msg';
-
-            if(match[2]) {
-                html += '<span class="nick">' + htmlencode.htmlEncode(match[2]) + '</span>';
-            } else {
-                msg_class = 'event';
-            }
-
-            var url_regex = /(https?\:\/\/)(?:[\w-]+\.)+[\w-]+(?:\/[^\s]*)?/g;
-            var msg = match[3];
-
-            var line = '';
-            var result;
-            while(result = url_regex.exec(msg)) {
-                if(result.index > 0) {
-                    line += htmlencode.htmlEncode(msg.substring(0, result.index));
+            var html = '';
+            html += '<html>\n';
+            html += '   <head>\n';
+            html += '       <title>#java-gaming logs ' + htmlencode.htmlEncode(date_string) + '</title>\n';
+            html += '       <style> body { line-height: 1.3em; background-color: #0C1010; color: #008000; font-family: "Consolas", "Source Code Pro", "Andale Mono", "Monaco", "Lucida Console", monospace; }\n';
+            html += '               a { color: #68A4DE; text-decoration: none; } a:hover { text-decoration: underline; }\n'
+            html += '               .msg { color: #FFFFFF; } .event { color: #BDB76B; } .nick { color: #DC143C; display: inline-block; text-align: right; min-width: 140px; max-width: 140px; padding-right: 10px; }</style>\n';
+            html += '   </head>\n';
+            html += '   <body>\n';
+            html += '       <h1>#java-gaming logs ' + htmlencode.htmlEncode(date_string) + '</h1>\n';
+            lines.forEach(function(s) {
+                var msg_regex = /^(\[.+?\])  (<.+?> )?(.+)$/;
+                var match = msg_regex.exec(s);
+                if(!match) {
+                    return;
                 }
 
-                line += '<a href="' + result[0] + '">' + htmlencode.htmlEncode(result[0]) + '</a>';
-                msg = msg.substring(result.index + result[0].length);
-            }
+                html += '       <div><span class="datestring">' + htmlencode.htmlEncode(match[1]) + '</span>  ';
 
-            if(msg.length > 0) {
-                line += htmlencode.htmlEncode(msg);
-            }
+                var msg_class = 'msg';
 
-            html += '<span class="' + msg_class + '">' + line + '</span></div>\n';
-        });
-        html += '   </body>\n';
-        html += '</html>\n';
+                if(match[2]) {
+                    html += '<span class="nick">' + htmlencode.htmlEncode(match[2]) + '</span>';
+                } else {
+                    msg_class = 'event';
+                }
 
-        response.end(html);
+                var url_regex = /(https?\:\/\/)(?:[\w-]+\.)+[\w-]+(?:\/[^\s]*)?/g;
+                var msg = match[3];
+
+                var line = '';
+                var result;
+                while(result = url_regex.exec(msg)) {
+                    if(result.index > 0) {
+                        line += htmlencode.htmlEncode(msg.substring(0, result.index));
+                    }
+
+                    line += '<a href="' + result[0] + '">' + htmlencode.htmlEncode(result[0]) + '</a>';
+                    msg = msg.substring(result.index + result[0].length);
+                }
+
+                if(msg.length > 0) {
+                    line += htmlencode.htmlEncode(msg);
+                }
+
+                html += '<span class="' + msg_class + '">' + line + '</span></div>\n';
+            });
+            html += '   </body>\n';
+            html += '</html>\n';
+
+            response.writeHead(200);
+            response.end(html);
+        } catch(e) {
+            response.writeHead(500);
+            response.end('Internal error: ' + e);
+        }
     });
 }
