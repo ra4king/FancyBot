0010acc001fb20904d3eef6c83229ffbce676ab9
Roi Atalla <roi@roiatalla.com>
2017-04-06 03:34:34 -0400

    Converted markov to using mongodb.

--- actions/markov.js
+++ actions/markov.js
@@ -3,23 +3,41 @@ module.exports = {
     destroy: destroy
 };
 
-const n = 2;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = Promise;
 
-var allMappings = {};
-var keyCount = 0;
-var lineCount = 0;
-var characterCount = 0;
+var mappingsSchema = new Schema({
+    input: { type: String, index: true, validate: (s) => s || s === null },
+    next: { type: String, validate: (s) => s || s === null },
+    nick: { type: String, required: true }
+});
+mappingsSchema.index({ nick: 1, input: 1 }, { background: true });
+try {
+    var Mapping = mongoose.model('Mapping', mappingsSchema);
+} catch(e) {
+    var Mapping = mongoose.model('Mapping');
+}
 
-var lastUserMappings = {}
-var lastUserName = null;
-
-var lastTimeout = null;
+var n;
+var lastTimeout;
 
 function init(action, utils, config) {
-    config.annoyingModeRate = config.annoyingModeRate || 0;
+    n = config.n = config.n || 2;
     utils.save_config();
 
-    allMappings = generateForUser(null);
+    mongoose.connect('mongodb://roiatalla.com:27017/markov', { user: config.user, pass: config.pwd }, (err) => {
+        if(err) {
+            console.error('Markov: Error connecting to MongoDB.');
+            console.error(err);
+            return;
+        }
+
+        console.log('Markov: Successully connected to MongoDB.');
+    });
+
+    config.annoyingModeRate = config.annoyingModeRate || 0;
+    utils.save_config();
 
     action({
             name: 'markov',
@@ -37,45 +55,32 @@ function init(action, utils, config) {
         bot.sayDirect(from, to, 'Annoying mode set with a rate of ' + config.annoyingModeRate + '%');
     });
 
-    // action({
-    //         name: 'speaklike',
-    //         help: 'Usage: !speaklike username. Give it a username and it will speak like them.'
-    //     },
-    //     function(bot, from, to, text) {
-    //         if(!text) {
-    //             bot.sayDirect(from, to, 'Give me a username!');
-    //             return;
-    //         }
+    action({
+        name: 'speaklike',
+        help: 'Usage: !speaklike username. Give it a username and it will speak like them.'
+    },
+    function(bot, from, to, text) {
+        if(!text) {
+            bot.sayDirect(from, to, 'Give me a username!');
+            return;
+        }
 
-    //         if(text != lastUserName) {
-    //             bot.sayDirect(from, to, 'Analyzing logs of ' + text + '...');
-    //             var mappings = generateForUser(text);
-    //             if(Object.keys(mappings).length != 0) {
-    //                 lastUserName = text;
-    //                 lastUserMappings = mappings;
-    //             } else {
-    //                 bot.sayDirect(from, to, 'No logs found for ' + text + '.');
-    //                 return;
-    //             }
-    //         }
-
-    //         var message = generateMarkov(lastUserMappings, null);
-    //         bot.sayDirect(from, to, false, message.join(' '))
-    //     });
+        generateMarkov(text, null, (err, message) => {
+            bot.sayDirect(from, to, false, err ? String(err) : message.join(' '));
+        });
+    });
 
     function onMessage(bot, from, to, text) {
-        createMapping(allMappings, text);
-
-        if(from == lastUserName) {
-            createMapping(lastUserMappings, text);
-        }
+        createMapping(from, text);
 
         if(text.indexOf(bot.nick) != -1 || (100 * Math.random()) < config.annoyingModeRate) {
             let split = text.split(' ');
             let idx = Math.floor(Math.random() * (split.length - n + 1));
             let input = split.slice(idx, idx + n);
-            var message = generateMarkov(allMappings, input);
-            setTimeout(() => bot.sayDirect(from, to, false, message.join(' ')), 700);
+
+            generateMarkov(null, input, (err, message) => {
+                setTimeout(() => bot.sayDirect(from, to, false, err ? String(err) : message.join(' ')), 700);
+            });
         }
     }
 
@@ -96,7 +101,7 @@ function init(action, utils, config) {
         markov(bot, '', bot.channel, '');
 
         var milliseconds = Math.floor(Math.random() * 4 * 60 * 60 * 1000) + 2 * 60 * 60 * 1000;
-        console.log('markov: waiting for ' + milliseconds + ' ms');
+        console.log('Markov: waiting for ' + milliseconds + ' ms');
 
         lastTimeout = setTimeout(sayRandomly, milliseconds);
     }
@@ -105,24 +110,22 @@ function init(action, utils, config) {
 }
 
 function destroy() {
-    allMappings = {};
-    lastUserMappings = {};
-    clearTimeout(lastTimeout);
+    mongoose.disconnect(() => console.log('Markov: Successfully disconnected from MongoDB.'));
 }
 
 function cleanString(str) {
     return str.trim().toLowerCase().replace(/[^\w,\.\/\\-]/g, '');
 }
 
-function addMapping(mappings, key, value) {
-    if(mappings[key]) {
-        mappings[key].push(value);
-    } else {
-        mappings[key] = [value];
-    }
+function addMapping(from, key, value) {
+    new Mapping({
+        input: key,
+        next: value,
+        nick: from
+    }).save();
 }
 
-function createMapping(mappings, text) {
+function createMapping(from, text) {
     var firstRun = true;
     var lastPieces = [];
     text.split(' ').forEach((piece) => {
@@ -136,11 +139,11 @@ function createMapping(mappings, text) {
             var value = piece;
 
             if(firstRun) {
-                addMapping(mappings, null, key);
+                addMapping(from, null, key);
                 firstRun = false;
             }
 
-            addMapping(mappings, key, value);
+            addMapping(from, key, value);
 
             lastPieces.shift();
         }
@@ -148,42 +151,9 @@ function createMapping(mappings, text) {
         lastPieces.push(piece);
     });
 
-    addMapping(mappings, JSON.stringify(lastPieces), null);
-}
-
-function generateForUser(user) {
-    var mappings = {};
-
-    const fs = require('fs');
-
-    fs.readdirSync('logs/').forEach((file) => {
-        if(!file.endsWith('.log')) return;
-
-        var contents = fs.readFileSync('logs/' + file).toString().split('\n');
-
-        contents.forEach((line) => {
-            var msg_regex = /^(\[.+?\])  ([<-](.+?)[>-] |\* )?(.+)$/;
-            var match = msg_regex.exec(line);
-            if(!match || (!user && !match[3]) || (user && match[3] !== user)) {
-                return;
-            }
-
-            var text = match[4];
-
-            if(!user) {
-                lineCount++;
-                characterCount += text.length;
-            }
-
-            createMapping(mappings, text);
-        });
-    });
-
-    if(!user) {
-        keyCount = Object.keys(mappings).length;
+    if(lastPieces.length != n) {
+        addMapping(from, JSON.stringify(lastPieces), null);
     }
-
-    return mappings;
 }
 
 function capitalize(prev, str) {
@@ -194,64 +164,123 @@ function capitalize(prev, str) {
     }
 }
 
-function generateMessage(mappings, min_length, initialInputs) {
+function generateMessage(user, initialInputs, min_length, callback) {
+    var generate = (keyString, keyArray) => {
+        var prev = null;
+        var message = keyArray.map((piece) => {
+            var s = capitalize(prev, piece);
+            prev = piece;
+            return s;
+        });
+
+        var generateNext = () => {
+            var query = { input: keyString };
+            if(user) {
+                query.nick = user;
+            }
+
+            Mapping.find(query, (err, mappings) => {
+                if(err) {
+                    console.error('Markov: error when getting mappings for input: ' + keyString);
+                    console.error(err);
+                    return callback(err);
+                }
+
+                if(mappings.length == 0) {
+                    return callback(null, message);
+                }
+
+                let tries = 5;
+                do {
+                    let nextIdx = Math.floor(Math.random() * mappings.length);
+                    var piece = mappings[nextIdx].next;
+                } while(piece == null && --tries > 0 && message.length < min_length);
+
+                if(!piece) {
+                    return callback(null, message);
+                }
+
+                message.push(capitalize(prev, piece));
+
+                keyArray.shift();
+                keyArray.push(piece);
+
+                prev = piece;
+                keyString = JSON.stringify(keyArray);
+
+                generateNext();
+            });
+        };
+
+        generateNext();
+    };
+
     if(initialInputs) {
         var keyArray = initialInputs.map(cleanString);
         var keyString = JSON.stringify(keyArray);
+        generate(keyString, keyArray);
     } else {
-        var initialIdx = Math.floor(Math.random() * mappings[null].length);
-        var keyString = mappings[null][initialIdx];
-        var keyArray = JSON.parse(keyString);
-    }
-
-    var prev = null;
-    var message = keyArray.map((piece) => {
-        var s = capitalize(prev, piece);
-        prev = piece;
-        return s;
-    });
-
-    do {
-        if(!mappings[keyString])
-            break;
-
-        let tries = 5;
-        do {
-            let nextIdx = Math.floor(Math.random() * mappings[keyString].length);
-            var piece = mappings[keyString][nextIdx];
-        } while(piece == null && --tries > 0 && message.length < min_length);
-
-        if(!piece) {
-            break;
+        var query = { input: null };
+        if(user) {
+            query.nick = user;
         }
 
-        message.push(capitalize(prev, piece));
+        Mapping.find(query).count((err, count) => {
+            if(err) {
+                console.error('Markov: error when getting null mappings.');
+                console.error(err);
+                return callback(err);
+            }
 
-        keyArray.shift();
-        keyArray.push(piece);
+            if(count == 0) {
+                return callback('No mappings for ' + user);
+            }
 
-        prev = piece;
-        keyString = JSON.stringify(keyArray);
-    } while(piece != null);
 
-    return message;
+            var initialIdx = Math.floor(Math.random() * count);
+
+            Mapping.find(query).skip(initialIdx).limit(1).exec((err, mapping) => {
+                if(err) {
+                    console.error('Markov: error when getting null mappings.');
+                    console.error(err);
+                    return callback(err);
+                }
+
+                var keyString = mapping[0].next;
+                var keyArray = JSON.parse(keyString);
+                generate(keyString, keyArray);
+            })
+        });
+    }
 }
 
-function generateMarkov(mappings, initialInputs) {
+function generateMarkov(user, initialInputs, callback) {
     const min_length = 6;
-
     var message = [];
-    do {
-        message = message.concat(generateMessage(mappings, min_length - message.length, initialInputs));
-        initialInputs = null;
 
-        let last = message[message.length - 1];
-        if(last && last.charAt(last.length - 1) != '.') {
-            message[message.length - 1] += '.';
-        }
-    } while(message.length < min_length);
+    var generateSentence = () => {
+        generateMessage(user, initialInputs, min_length - message.length, (err, sentence) => {
+            if(err) {
+                return callback(err);
+            }
 
-    return message;
+            message = message.concat(sentence);
+            initialInputs = null;
+
+            let last = message[message.length - 1];
+            if(last && last.charAt(last.length - 1) != '.') {
+                message[message.length - 1] += '.';
+            }
+            
+            if(message.length < min_length) {
+                generateSentence();
+            } else {
+                callback(null, message);
+            }
+        });
+    };
+
+    generateSentence();
 }
 
 function markov(bot, from, to, text) {
@@ -262,7 +291,8 @@ function markov(bot, from, to, text) {
 
         switch(split[0]) {
             case 'stats':
-                bot.sayDirect(from, to, 'Line count: ' + lineCount + '. Character count: ' + characterCount + '. Key count: ' + keyCount);
+                bot.sayDirect(from, to, 'Stats to be implemented...');
+                //bot.sayDirect(from, to, 'Line count: ' + lineCount + '. Character count: ' + characterCount + '. Key count: ' + keyCount);
                 return;
             default:
                 if(split.length != n) {
@@ -274,6 +304,7 @@ function markov(bot, from, to, text) {
         }
     }
 
-    var message = generateMarkov(allMappings, initialInputs);
-    bot.sayDirect(from, to, false, message.join(' '));
+    generateMarkov(null, initialInputs, (err, message) => {
+        bot.sayDirect(from, to, false, err ? String(err) : message.join(' '));
+    });
 }
