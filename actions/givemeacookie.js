c72a6ca43de78cce1d3255c46baeecb1d33925bc
Secrets2L <brynnbrancamp@gmail.com>
2017-05-10 19:36:34 -0500

    removed bad people

--- actions/givemeacookie.js
+++ actions/givemeacookie.js
@@ -9,28 +9,20 @@ function init(action, utils, config) {
         name: '!givemeacookie',
         help: 'Usage: !givemeacookie',
         op_only: true,
-        explicitly_allowed_users: ['secrets', 'drenius', 'waratte']
+        explicitly_allowed_users: ['secrets']
     };
 
     action(options, give_me_a_cookie);
 }
 
 function give_me_a_cookie(bot, from, to, text, message, utils, config) {
-    var bad_people = ['drenius', 'waratte'];
 
-    //is this person subjectively bad?
-    if(bad_people.indexOf(from) == -1) {
+    //normal operations
+    var types = ['big', 'yummy', 'scrumptious', 'hot', 'crunchy', 'oatmeal', 'chocolate'];
+    bot.action(to === bot.nick ? from : to, 'gives a ' + utils.choose_random(types) + ' cookie to ' + from);
 
-        //normal operations
-        var types = ['big', 'yummy', 'scrumptious', 'hot', 'crunchy', 'oatmeal', 'chocolate'];
-        bot.action(to === bot.nick ? from : to, 'gives a ' + utils.choose_random(types) + ' cookie to ' + from);
-
-        //is this cookie baked?
-        if(Math.random() < 0.1) {
-            bot.action(to === bot.nick ? from : to, 'Heeey mannn. I added a reaal extra special ingreddient to that cookie :}. hahaaa');
-        }
-
-    } else {
-        bot.action(to === bot.nick ? from : to, 'gives a jizz filled cookie to ' + from);
+    //is this cookie baked?
+    if(Math.random() < 0.1) {
+        bot.action(to === bot.nick ? from : to, 'Heeey mannn. I added a reaal extra special ingreddient to that cookie :}. hahaaa');
     }
 }
