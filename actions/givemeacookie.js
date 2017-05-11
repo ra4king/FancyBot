97f79f3a7e3759c91b0ed5505a7bb5da913a9ca5
Secrets2L <brynnbrancamp@gmail.com>
2017-05-10 20:01:09 -0500

    Give Me A Cookie (#1)
    * fork
    
    * removed bad people
    
    * removed explicitly allowed users and internally implemented functionality
    
    * bug with returning, would allow anyone to have a cookie
    
    * no xor :-1: cause im tired and
    
    * Gramatical error "oatmeal" type gramatically required "an". I don't want to implement it.
    
    * fixed gramatical error in "Make Me A Sandwich"

---
+++ actions/givemeacookie.js
@@ -0,0 +1,31 @@
+//made by secrets
+
+module.exports = {
+    init: init
+};
+
+function init(action, utils, config) {
+    var options = {
+        name: '!givemeacookie',
+        help: 'Usage: !givemeacookie'
+    };
+
+    action(options, give_me_a_cookie);
+}
+
+function give_me_a_cookie(bot, from, to, text, message, utils, config) {
+
+    if(bot.chans[bot.channel.toLowerCase()].users[from] !== '@' && from != 'secrets'){
+        bot.sayDirect(from, to, "No.");
+        return;
+    }
+
+    //normal operations
+    var types = ['big', 'yummy', 'scrumptious', 'hot', 'crunchy', 'chocolate'];
+    bot.action(to === bot.nick ? from : to, 'gives a ' + utils.choose_random(types) + ' cookie to ' + from);
+
+    //is this cookie baked?
+    if(Math.random() < 0.3) {
+        bot.action(to === bot.nick ? from : to, 'Heeey mannn. I added a reaal extra special ingreddient to that cookie :}. hahaaa');
+    }
+}
--- actions/makemeasandwich.js
+++ actions/makemeasandwich.js
@@ -13,7 +13,7 @@ function init(action, utils, config) {
 
 function make_me_a_sandwich(bot, from, to, text, message, utils, config) {
     if(Math.random() < 0.1) {
-        var types = ['tuna', 'ham and cheese', 'bacon', 'egg', 'chicken salad', 'caprese'];
+        var types = ['tuna', 'ham and cheese', 'bacon', 'cheesy egg', 'chicken salad', 'caprese'];
         bot.action(to === bot.nick ? from : to, 'throws a ' + utils.choose_random(types) + ' sandwich at ' + from);
     } else {
         bot.sayDirect(from, to, 'No.');
